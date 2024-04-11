109544cb74b49372a5a62de9ee83e4f76cdf764d
Laxman Dhulipala <laxmandhulipala@gmail.com>
2024-04-11 18:05:39 -0400

    commit garbage

--- scripts/run_static.sh
+++ scripts/run_static.sh
@@ -2,35 +2,35 @@
 declare -a undir_graph=(
   # Social
 
-  "com-orkut_sym"
-  "soc-LiveJournal1_sym"
-  "twitter_sym"
-  "friendster_sym"
-
-  # # Web
-  "sd_arc_sym"
-
-
-  # # Road
-
-  "RoadUSA_sym"
-  "Germany_sym"
+#  "com-orkut_sym"
+#  "soc-LiveJournal1_sym"
+#  "twitter_sym"
+#  "friendster_sym"
+#
+#  # # Web
+#  "sd_arc_sym"
+#
+#
+#  # # Road
+#
+#  "RoadUSA_sym"
+#  "Germany_sym"
 
   # # k-NN
   "Household.lines_5_sym"
-  "CHEM_5_sym"
-  "GeoLifeNoScale_2_sym"
-  "GeoLifeNoScale_5_sym"
-  "GeoLifeNoScale_10_sym"
-  "GeoLifeNoScale_15_sym"
-  "GeoLifeNoScale_20_sym"
-  "Cosmo50_5_sym"
-
-  # # Synthetic
-  "grid_1000_10000_03_sym"
-  "grid_1000_10000_sym"
-  "grid_4000_4000_03_sym"
-  "grid_4000_4000_sym"
+#  "CHEM_5_sym"
+#  "GeoLifeNoScale_2_sym"
+#  "GeoLifeNoScale_5_sym"
+#  "GeoLifeNoScale_10_sym"
+#  "GeoLifeNoScale_15_sym"
+#  "GeoLifeNoScale_20_sym"
+#  "Cosmo50_5_sym"
+#
+#  # # Synthetic
+#  "grid_1000_10000_03_sym"
+#  "grid_1000_10000_sym"
+#  "grid_4000_4000_03_sym"
+#  "grid_4000_4000_sym"
 
 )
 
